401398e100381ed50728787c0c87206694f11dcc
Kishore Kumar <kishore1234500000@gmail.com>
2022-01-06 14:12:26 +0530

    add some more functions

--- notes.js
+++ notes.js
@@ -1,10 +1,29 @@
-import fs from  'fs'
+import fs from 'fs'
 const loadNotes = function(){
     try{
-        return fs.readFileSync(notes.json,'utf8');
+        return JSON.parse(fs.readFileSync("./notes.json",'utf8'));
     }catch{
         return {notes:[]};
     }
 }
 
-module.exports ={};
+const saveNotes = function(notes){
+    fs.writeFileSync("./notes.json",JSON.stringify(notes));
+}
+
+const addNote = function(data,notes){
+    notes.notes.append(data);
+    return notes;
+}
+
+const removeNote = function(notes){
+    notes.notes.pop();
+    return notes;
+}
+
+export {
+    loadNotes,
+    saveNotes,
+    addNote,
+    removeNote
+};
