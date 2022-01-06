e1d65a44a93448f0443f8532518481b29715d0b9
Kishore Kumar <kishore1234500000@gmail.com>
2022-01-06 12:48:35 +0530

    add loadNotes function and .gitignore

--- notes.js
+++ notes.js
@@ -1,7 +1,10 @@
 import fs from  'fs'
-
-
-
-
+const loadNotes = function(){
+    try{
+        return fs.readFileSync(notes.json,'utf8');
+    }catch{
+        return {notes:[]};
+    }
+}
 
 module.exports ={};
